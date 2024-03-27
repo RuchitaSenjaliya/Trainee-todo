ae36e426f448c6096a08edc5dd24e8d03ca48e16
Ruchita <senjaliyaruchita@gmail.com>
2024-03-27 15:22:56 +0530

    disable

--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -4,8 +4,11 @@ import { useParams } from "react-router-dom";
 export default function TodoDetail() {
   const params = useParams();
   console.log(params);
+
   const allTodoData = JSON.parse(localStorage.getItem("tasks"));
+
   const currentTask = allTodoData.filter((item) => item.id === params.todoId);
+
   console.log(currentTask);
   return (
     <div>
